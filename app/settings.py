9fc6b8486ad26b10d9c34356024a0765ff698ef6
Akaywonder1124 <akaycopy@gmail.com>
2023-11-06 14:29:27 +0100

    commit

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,8 @@
 # DATABASE_USER = os.environ.get("DATABASE_USER")
 # DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
 
+# this is a free postgreSQL database,
+#  for the sake of this quick project, I didn,t save the login in an enviroment project
 DATABASE_USER = "zhkojnfg"
 DATABASE_PASSWORD = "mDkMdVxhChPNsNYDcIZX_jPCLLkdsPGg"
 
