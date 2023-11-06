40f9b7bfb6b167487a0300088b218b106b6bdac0
Akaywonder1124 <akaycopy@gmail.com>
2023-11-06 14:26:05 +0100

    commit

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,9 @@
 # DATABASE_USER = os.environ.get("DATABASE_USER")
 # DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
 
-DATABASE_USER = "admin"
-DATABASE_PASSWORD = "Olayemi04"
+DATABASE_USER = "zhkojnfg"
+DATABASE_PASSWORD = "mDkMdVxhChPNsNYDcIZX_jPCLLkdsPGg"
 
 DATABASE_URL = (
-    f"postgres://{DATABASE_USER}:{DATABASE_PASSWORD}@localhost:5432/saydatadb"
+    f"postgres://{DATABASE_USER}:{DATABASE_PASSWORD}@mahmud.db.elephantsql.com/zhkojnfg"
 )
